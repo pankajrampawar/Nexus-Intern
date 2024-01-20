c8a60c9e4e3bc88783d3389a9fcc6a6e7fe8918f
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-20 10:39:35 +0530

    Update server configuration and models

--- server/src/config/dbConfig.js
+++ server/src/config/dbConfig.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
 export const configDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    const conn = await mongoose.connect(`${process.env.MONGO_URL}/${DB_NAME}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
