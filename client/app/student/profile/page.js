06b50385db9ad3fca7df8f6db40373802e5e8925
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-21 01:45:53 +0530

    profile image working

--- client/app/student/profile/page.js
+++ client/app/student/profile/page.js
@@ -16,34 +16,8 @@ export default function Profile() {
 
     return (
         <main className="bg-primary">
-            <section>
-                <div>
-                    <img src={userData.profileImage}/>
-                </div>
-
-                <div>
-                    <div>
-                        <h1>{userData.name}</h1>
-                        <h1>{userData.email}</h1>
-                        <h1>{userData.phone}</h1>
-                        <h2>{userData.college}</h2>
-                    </div>
-
-                    <div>
-                        <h2>{userData.resume}</h2>
-                    </div>
-                    <div>
-                        {userData.skills.map((skill) => {
-                            return (<div>
-                                {skill}
-                                </div>)
-                        })}
-                    </div>
-
-                    <div>
-                        <h2>{userData.education}</h2>
-                    </div>
-                </div>
+            <section className='min-w-[700px]'>
+                hello
             </section>
         </main>
     )
