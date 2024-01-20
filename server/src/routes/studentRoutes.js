34228d468962236fa290ba770a0d108fbeb30c86
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-20 23:35:34 +0530

    Update import statements for models in controllers

--- server/src/routes/studentRoutes.js
+++ server/src/routes/studentRoutes.js
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from "express";
 import {
-    applyForInternship,
-    editBio,
-    editSkills,
-    getStudentInfo
-} from '../controllers/studentController'
+  applyForInternship,
+  editBio,
+  editSkills,
+  getStudentInfo,
+} from "../controllers/studentController.js";
 
 const studentRouter = express.Router();
 
-studentRouter.get("StudentInfo", getStudentInfo)
+studentRouter.get("StudentInfo", getStudentInfo);
 studentRouter.post("/applyForInternship", applyForInternship);
 studentRouter.post("/editBio", editBio);
 studentRouter.post("/editSkills", editSkills);
