45ca06a9f513e49b95996646f10adc8cfd3e7f51
Latish Adwani <104522651+Latish705@users.noreply.github.com>
2024-01-21 01:42:03 +0530

    adding formdata in signup

--- client/app/action.js
+++ client/app/action.js
@@ -1,18 +1,28 @@
-import axios from 'axios'
+import axios from "axios";
 
 export const getAllInternships = async () => {
-    try {
-        const response = await axios.get('http://localhost:8080/api/internships/getAllInternships')
-        console.log(response.data.internships);
-        return response.data.internships;
-    } catch (error) {
-        alert("unable to get internships internal server error");
-        console.log(error);
-    }
-}
+  try {
+    const response = await axios.get(
+      "http://localhost:8080/api/internships/getAllInternships"
+    );
+    console.log(response.data.internships);
+    return response.data.internships;
+  } catch (error) {
+    alert("unable to get internships internal server error");
+    console.log(error);
+  }
+};
 
-export const studentLogin = async (data) => {
-    try {
-        /// adding api request here
-    }
-}
+export const studentSignup = async (data) => {
+  try {
+    const formData = new FormData();
+    formData.append("email", data.email);
+    formData.append("password", data.password);
+    formData.append("fullName", data.fullName);
+    formData.append("phone", data.phone);
+    formData.append("info", data.info);
+    formData.append("profileImage", data.profileImage);
+  } catch (error) {
+    console.log(error);
+  }
+};
