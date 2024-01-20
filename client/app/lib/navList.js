f7ba64172d7cba33affd5254271e4ee0b0b2fcc5
pankaj ram pawar <pankajpawars123@gmail.com>
2024-01-20 09:49:05 +0530

    sidebar and navlist added

---
+++ client/app/lib/navList.js
@@ -0,0 +1,18 @@
+export const navList = [
+    {
+        name: "Home",
+        img: "/home.svg",
+    },
+    {
+        name: "Profile",
+        img: "/profile.svg",
+    },
+    {
+        name: "Internships",
+        img: "/editLocation.svg",
+    },
+    {
+        name: 'filter',
+        img: '/filter.svg'
+    }
+]
--- client/app/ui/sidebar.js
+++ client/app/ui/sidebar.js
@@ -1,9 +1,40 @@
 'use client'
 
+import React from 'react';
+import { navList } from '../lib/navlilst';
+
+
 export default function Sidebar() {
+
+    const Navitems = () => {
+        return <div className='flex flex-col items-center gap-8'>
+            {
+                navList.map((element) => {
+                    return (
+                        <div className='flex flex-col items-center text-white font-semibold text-sm'>
+                            <div>
+                                <img src={element.img} alt={element.name} className='invert' />
+                            </div>
+                            <div>
+                                {element.name}
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    }
+
     return (
-        <main>
-            this is side bar
+        <main className='bg-primary text-white max-w-[200px] text-center h-full rounded-2xl p-3 pr-3 '>
+            <header className='mt-5 w-full flex justify-center'>
+                <div>
+                    Samarho Sarthi
+                </div>
+            </header>
+            <section className='mt-10'>
+                <Navitems/>
+            </section>
         </main>
     )
 }
